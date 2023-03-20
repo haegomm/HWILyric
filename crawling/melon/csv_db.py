698032e965ff92696c50417db8dba1da834521d2
dokkyunYU <ydgar001@naver.com>
2023-03-20 23:13:05 +0900

    Feat : add melon_newsong_crawler.py + csv_db.py

---
+++ crawling/melon/csv_db.py
@@ -0,0 +1,84 @@
+import os
+import pandas as pd
+import pymysql
+import re
+import traceback
+# DB Information
+host = "j8b107.p.ssafy.io"
+user = "root"
+password = "hwilYRIC107"
+database = "hwilyric"
+
+# MySQL Connection
+conn = pymysql.connect(host=host, user=user, password=password, db=database, charset="utf8")
+curs = conn.cursor(pymysql.cursors.DictCursor)
+
+
+# DB insert
+sql = 'INSERT INTO music (title,artist,album,released_date,genre,`rank`,chart_year,lyrics) VALUES '
+
+columns = ['title','artist','album','released_date','genre','rank','chart_year']
+
+# csv파일 경로 설정
+path = "./"
+musics = "musics_new.csv"
+lyrics = "lyrics_new.csv"
+
+# csv 파일 읽기
+
+musics_file = pd.read_csv(path+musics, encoding="utf8")
+
+musics_length = len(musics_file)
+
+print(musics_length)
+
+lyrics_file = open(path+lyrics, encoding="utf8")
+lyrics_lines = lyrics_file.readlines()
+
+values_cnt = 0
+each_sql = ''
+for i in range(musics_length):
+    try:
+        each_sql += "("
+        for column in columns:
+            if column == 'released_date':
+                if len(musics_file[column][i]) < 5:
+                    if musics_file[column][i] == '-':
+                        each_sql += "\"" + str(musics_file[column][i+3]) + "-01-01" + "\""
+                    else:
+                        each_sql += "\"" + str(musics_file[column][i]) + "-01-01" + "\""
+                else:
+                    date_list = musics_file[column][i].split('.')
+                    try:
+                        each_sql += "\"" + date_list[0] + '-' + date_list[1] + '-' + date_list[2] + "\""
+                    except:
+                        each_sql += "\"" + date_list[0] + '-' + date_list[1] + '-' + "01" + "\""
+            elif column == 'rank' or column == 'chart_year':
+                each_sql += str(musics_file[column][i])
+            # elif column == 'genre' and ',' in musics_file[column][i]:
+            #     each_sql += "\"" + musics_file[column][i] + "\""
+            else:
+                each_sql += "\"" + str(musics_file[column][i]) + "\""
+            each_sql += ', '
+        if "\"" in lyrics_lines[2 * (i + 1)]:
+            lyrics_lines[2 * (i + 1)] = lyrics_lines[2 * (i + 1)].replace("\"", "")
+        each_sql += "\"" + lyrics_lines[2 * (i + 1)].strip() + "\""
+        each_sql += ")"
+        values_cnt += 1
+        if values_cnt == 100:
+            curs.execute(str(sql + each_sql))
+            values_cnt = 0
+            each_sql = ''
+            continue
+        each_sql += ", "
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+        break
+
+if values_cnt != 0:
+    curs.execute(str(sql + each_sql.rstrip(", ")))
+
+conn.commit()
+curs.close()
+conn.close()
---
+++ crawling/melon/melon_newsong_crawler.py
@@ -0,0 +1,127 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as bs
+import pandas as pd 
+from collections import OrderedDict
+import time
+import csv
+import traceback
+from datetime import datetime, timedelta
+
+# datetime.today()            # 현재 날짜 가져오기
+
+# datetime.today().year        # 현재 연도 가져오기
+
+# datetime.today().month      # 현재 월 가져오기
+
+# datetime.today().day        # 현재 일 가져오기
+
+# datetime.today().hour        # 현재 시간 가져오기
+
+# datetime.today().strftime("%Y%m%d%H%M%S")    # YYYYmmddHHMMSS 형태의 시간 출력
+
+# datetime.today().strftime("%Y/%m/%d %H:%M:%S")  # YYYY/mm/dd HH:MM:SS 형태의 시간 출력
+
+# from datetime import datetime, 
+
+
+
+# yesterday = datetime.today() - timedelta(1)
+
+
+time_to_wait = 10
+time_to_sleep = 4
+TIMEDELTA = 2
+TODAY = datetime.today().strftime("%Y.%m.%d")
+TARGET_DATE = (datetime.today() - timedelta(TIMEDELTA)).strftime("%Y.%m.%d")
+BASE_URL = 'https://www.melon.com/new/index.htm'
+DETAIL_URL = 'https://www.melon.com/song/detail.htm?songId='
+BASE_TERM = 50
+order_num = 0
+
+
+
+# print(TODAY, TARGET_DATE)
+
+
+
+driver = webdriver.Chrome('C:/Users/SSAFY/Desktop/semester2/crawling/chromedriver.exe')
+
+# https://www.melon.com/new/index.htm#params%5BareaFlg%5D=I&po=pageObj&startIndex=1
+
+
+# 한 페이지의 노래를 전부 받아올 리스트 초기화
+musics_list = [['title', 'artist', 'album', 'release_date', 'genre', 'rank', 'chart_year']]
+lyrics_list = [['lyrics']]
+is_end = False
+
+while (not is_end):
+    try:
+        # 최신 곡 차트 찾아 들어가기
+        # order_num은 숫자를 50씩 증가시키기 위한 것
+        url = BASE_URL + '#params%5BareaFlg%5D=I&po=pageObj&startIndex=' + str(order_num * BASE_TERM + 1)
+        driver.get(url)
+        driver.implicitly_wait(time_to_wait)
+        time.sleep(time_to_sleep)
+
+        # 로드한 페이지 소스를 전부 불러와서 뷰티풀수프로 파싱
+        html = driver.page_source
+        soup = bs(html, 'html.parser')
+
+        songlist_soup = soup.select('tbody > tr')
+
+        length = len(songlist_soup)
+
+        for i in range(length):
+            song_num = songlist_soup[i].select('td')[3].select_one('div.wrap > a')['href'].split('\'')[1]
+            title = songlist_soup[i].select_one('div.wrap_song_info > div.ellipsis > span > a').get_text()
+            url = DETAIL_URL + song_num
+            driver.get(url)
+            driver.implicitly_wait(time_to_wait)
+            time.sleep(time_to_sleep)
+            html = driver.page_source
+            song_soup = bs(html, 'html.parser')
+            # csv_col = ['title', 'artist', 'album', 'release_date', 'genre', 'rank', 'chart_year']
+            # print(song_num)
+            artist = song_soup.select_one('div.info > div.artist > a > span').get_text()
+            meta_data = song_soup.select('div.meta > dl.list > dd')
+            album = meta_data[0].select_one('a').get_text()
+            release_date = meta_data[1].get_text()
+            if (release_date == TARGET_DATE):
+                is_end = True
+                break
+            genre = meta_data[2].get_text()
+            rank = 'NULL'
+            chart_year = 'NULL'
+            try:
+                lyrics = song_soup.select_one('#d_video_summary').get_text('\n').strip('\n\t').split('\n')
+            except:
+                lyrics = ['가사 정보 없음']
+            musics_list.append([title, artist, album, release_date, genre, rank, chart_year])
+            lyrics_list.append(lyrics)
+        
+
+        order_num += 1
+    except Exception as e:
+        print("예외 : ", e)
+        traceback.print_exc()
+        is_end = True
+        break
+
+
+f_musics = open("musics_newly.csv", "w", encoding="utf8")
+writer = csv.writer(f_musics)
+writer.writerows(musics_list)
+f_musics.close()
+
+
+f_lyrics = open("lyrics_newly.csv", "w", encoding="utf8")
+writer = csv.writer(f_lyrics)
+writer.writerows(lyrics_list)
+f_lyrics.close()
+
+
+
+
