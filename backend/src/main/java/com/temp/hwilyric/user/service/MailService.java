21a8cc3d963c5ebc22ab1993a8aac4f203864523
jeongjuhwang <hjj950303@naver.com>
2023-03-20 15:44:43 +0900

    fix: modify email design for signup and reissuing password, comment out mongoDB dependency

--- backend/src/main/java/com/temp/hwilyric/user/service/MailService.java
+++ backend/src/main/java/com/temp/hwilyric/user/service/MailService.java
@@ -32,24 +32,28 @@ public class MailService {
 
         String msg = "";
         msg += "<div style='margin: 20px; width: 70%; text-align: center'>";
-        msg += "<hr style='border: solid 5px #fbd5e0'/>";
+        msg += "<hr style='border: solid 5px #B0E3F9'/>";
+        msg += "<img style='width: 100%' src='https://holorok-hwilyric-bucket.s3.ap-northeast-2.amazonaws.com/profile/hwilyric_banner.png'/>"; // 해당 경로는 S3에 올린 후 변경 예정
         msg += "<br />";
         msg += "<br />";
-        msg += "<img style='width: 15%' src='https://drive.google.com/file/d/1qILXGmoECJrsD9GJT5ag-DBchvt-EYJu/view?usp=sharing'/>"; // 해당 경로는 S3에 올린 후 변경 예정
-        msg += "<h1>인증코드 안내 이메일 입니다.</h1>";
+        msg += "<h2>인증코드 안내 이메일 입니다.</h2>";
         msg += "<br />";
         msg += "<br />";
-        msg += "<p>안녕하세요.</p>";
-        msg += "<p>휘리릭을 이용해 주셔서 진심으로 감사드립니다.</p>";
-        msg += "<p>아래 코드를 입력하여 회원가입을 완료해 주세요.</p>";
+        msg += "<p>🎵 안녕하세요 🎵</p>";
         msg += "<br />";
-        msg += "<div style='margin: 0 auto; text-align: center; width: 300px; border: 1px solid black; font-family: verdana; padding: 10px;'>";
-        msg += "<div style='color: #765790; font-size: 130%'><strong>";
+        msg += "<p>휘리릭을 이용해 주셔서 진심으로 감사드립니다 💛</p>";
+        msg += "<p>아래 코드를 입력하여 회원가입을 완료해 주세요!</p>";
+        msg += "<br />";
+        msg += "<div style='margin: 0 auto; text-align: center; width: 300px; border: none; font-family: verdana; padding: 10px;'>";
+        msg += "<div style='font-size: 130%'><strong>";
         msg += code + "</strong></div>";
         msg += "</div>";
         msg += "<br />";
         msg += "<br />";
-        msg += "<hr style='border: solid 5px #fbd5e0'/>";
+        msg += "<img style='width: 25%' src='https://holorok-hwilyric-bucket.s3.ap-northeast-2.amazonaws.com/profile/hwilyric_logo.png'/>";
+        msg += "<br />";
+        msg += "<br />";
+        msg += "<hr style='border: solid 5px #B0E3F9'/>";
         msg += "</div>";
 
         MailDto mailDto = new MailDto();
@@ -79,24 +83,28 @@ public class MailService {
         }
         String msg = "";
         msg += "<div style='margin: 20px; width: 70%; text-align: center'>";
-        msg += "<hr style='border: solid 5px #fbd5e0'/>";
+        msg += "<hr style='border: solid 5px #B0E3F9'/>";
+        msg += "<img style='width: 100%' src='https://holorok-hwilyric-bucket.s3.ap-northeast-2.amazonaws.com/profile/hwilyric_banner.png'/>"; // 해당 경로는 S3에 올린 후 변경 예정
         msg += "<br />";
         msg += "<br />";
-        msg += "<img style='width: 15%' src='https://drive.google.com/file/d/1qILXGmoECJrsD9GJT5ag-DBchvt-EYJu/view?usp=sharing'/>"; // 해당 경로는 S3에 올린 후 변경 예정
         msg += "<h1>임시 비밀번호 안내 이메일 입니다.</h1>";
         msg += "<br />";
         msg += "<br />";
-        msg += "<p>안녕하세요.</p>";
-        msg += "<p>휘리릭을 이용해 주셔서 진심으로 감사드립니다.</p>";
-        msg += "<p>아래 임시 비밀번호로 로그인 후 비밀번호 변경을 해주세요.</p>";
+        msg += "<p>🎵 안녕하세요 🎵</p>";
         msg += "<br />";
-        msg += "<div style='margin: 0 auto; text-align: center; width: 300px; border: 1px solid black; font-family: verdana; padding: 10px;'>";
+        msg += "<p>휘리릭을 이용해 주셔서 진심으로 감사드립니다 💛</p>";
+        msg += "<p>아래 임시 비밀번호로 로그인 후 비밀번호 변경을 해주세요!</p>";
+        msg += "<br />";
+        msg += "<div style='margin: 0 auto; text-align: center; width: 300px; border: none; font-family: verdana; padding: 10px;'>";
         msg += "<div style='color: #765790; font-size: 130%'><strong>";
         msg += code + "</strong></div>";
         msg += "</div>";
         msg += "<br />";
         msg += "<br />";
-        msg += "<hr style='border: solid 5px #fbd5e0'/>";
+        msg += "<img style='width: 25%' src='https://holorok-hwilyric-bucket.s3.ap-northeast-2.amazonaws.com/profile/hwilyric_logo.png'/>";
+        msg += "<br />";
+        msg += "<br />";
+        msg += "<hr style='border: solid 5px #B0E3F9'/>";
         msg += "</div>";
 
         MailDto mailDto = new MailDto();
