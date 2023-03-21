5e64d0c93a9859f6a4fc130cf0ba8f5faa6017f3
leegaeun <rkdms4054@gmail.com>
2023-03-21 23:06:01 +0900

    feat: add progressbar and styled component and video current time

--- frontend/hwilyric/src/components/write/features/VideoPlayer.tsx
+++ frontend/hwilyric/src/components/write/features/VideoPlayer.tsx
@@ -1,77 +1,117 @@
 import React, { useState, useRef } from "react";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import YouTube from "react-youtube";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+
 import { PlayVideoId } from "../../../atoms/YoutubeVideoAtoms";
+import { PlayerBox, PlayerVideoBox, PlayButton } from "../../../styles/VideoPlayerStyles";
 
 type PlayerState = "playing" | "paused" | "stopped" | "unstarted";
 
 function VideoPlayer() {
-  const [playerState, setPlayerState] = useState<PlayerState>("unstarted");
-  const playerRef = useRef<YT.Player | null>(null);
-  const [videoId, setVideoId] = useRecoilState(PlayVideoId)
+  const [playerState, setPlayerState] = useState<PlayerState>("unstarted")
+  const playerRef = useRef<YT.Player | null>(null)
+  const videoId = useRecoilValue(PlayVideoId)
+
+  const [isPlay, setIsPlay] = useState(false)
+  const toggleIsPlay = () => setIsPlay(!isPlay)
+
+  const [currentTime, setCurrentTime] = useState(0);
+
 
   const handlePlayerReady = (event: YT.PlayerEvent) => {
-    console.log("Player is ready");
-    playerRef.current = event.target;
-  };
+    console.log("Player is ready")
+    playerRef.current = event.target
+  }
 
   const handlePlayerStateChange = (event: YT.OnStateChangeEvent) => {
-    console.log("Player state has changed", event.data);
-    setPlayerState(getPlayerState(event.data));
-  };
+    console.log("Player state has changed", event.data)
+    onStateChange(event)
+    setPlayerState(getPlayerState(event.data))
+  }
 
   const getPlayerState = (state: number): PlayerState => {
     switch (state) {
       case YT.PlayerState.PLAYING:
         return "playing";
       case YT.PlayerState.PAUSED:
+        onPlayerStop()
         return "paused";
       case YT.PlayerState.ENDED:
       case YT.PlayerState.CUED:
+        onPlayerStop()
         return "stopped";
       default:
         return "unstarted";
     }
-  };
+  }
 
   const handlePlay = () => {
     if (playerRef.current) {
-      playerRef.current.playVideo();
+      playerRef.current.playVideo()
+      toggleIsPlay()
     }
-  };
+  }
 
   const handlePause = () => {
     if (playerRef.current) {
-      playerRef.current.pauseVideo();
+      playerRef.current.pauseVideo()
+      toggleIsPlay()
     }
-  };
+  }
 
-  const handleStop = () => {
-    if (playerRef.current) {
-      playerRef.current.stopVideo();
-      setPlayerState("stopped");
+  // const handleStop = () => {
+  //   if (playerRef.current) {
+  //     playerRef.current.stopVideo();
+  //     setPlayerState("stopped");
+  //   }
+  // };
+
+  let intervalId: any
+  const onStateChange = (event) => {
+    if (event.data === YouTube.PlayerState.PLAYING) {
+      // setInterval 함수를 사용하여 100ms마다 현재 재생 시간을 업데이트합니다.
+      intervalId = setInterval(() => {
+        setCurrentTime(event.target.getCurrentTime());
+      }, 100);
     }
-  };
+  }
 
+  const onPlayerStop = () => {
+    clearInterval(intervalId)
+  }
   
   return (
-      
-      <div>
+      <PlayerBox>
         {/* <script src="https://www.youtube.com/iframe_api"></script> */}
-        <button onClick={handlePlay} disabled={playerState !== "paused"}>
-            ▶
-        </button>
-        <button onClick={handlePause} disabled={playerState !== "playing"}>
-            ⏸
-        </button>
-        <div>
-            <YouTube
+        <PlayerVideoBox>
+        <YouTube
+            opts={{
+              width: "70",
+              height: "70",
+              playerVars: {
+                rel: 0,
+                modestbranding: 1
+              }
+            }}
             videoId={videoId}
             onReady={handlePlayerReady}
             onStateChange={handlePlayerStateChange}
+            onEnd={(e)=>{e.target.stopVideo(0);}} 
             />
-        </div>
-    </div>
+        </PlayerVideoBox>
+        {/* <ProgressBar
+          now={currentTime}
+          max={props.duration}
+          label={`${Math.floor(currentTime)}s`}
+        /> */}
+        { isPlay ? (<PlayButton onClick={handlePause}>
+              ||
+        </PlayButton>) : (<PlayButton onClick={handlePlay}>
+              ▶
+          </PlayButton>)}
+    </PlayerBox>
   );
 }
 
