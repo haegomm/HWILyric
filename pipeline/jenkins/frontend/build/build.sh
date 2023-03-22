5cc5b42058658aa3cef0ed802af15b53cb34c81e
손민혁 <sonmh79@naver.com>
2023-03-22 23:25:05 +0900

    feat: add pipeline settings for dev-front

---
+++ pipeline/jenkins/frontend/build/build.sh
@@ -0,0 +1,16 @@
+echo "*******************************"
+echo "*******BUILDING FRONTEND*******"
+echo "*******************************"
+
+# frontend 빌드
+cd /var/jenkins_home/workspace/frontend-pipeline/frontend/hwilyric
+npm install
+npm run build
+
+echo "********************************"
+echo "***BUILDING DOCKER CONTAINERS***"
+echo "********************************"
+
+# docker container 빌드
+cd /var/jenkins_home/workspace/frontend-pipeline/pipeline/frontend
+docker compose build --no-cache
---
+++ pipeline/jenkins/frontend/deploy/deploy.sh
@@ -0,0 +1,12 @@
+# deploy.sh
+
+echo "*********************************"
+echo "***DEPLOYING DOCKER CONTAINERS***"
+echo "*********************************"
+
+# docker container 실행
+cd /var/jenkins_home/workspace/frontend-pipeline/pipeline/frontend
+docker compose up -d
+
+# dangling images 삭제
+docker image prune -f
