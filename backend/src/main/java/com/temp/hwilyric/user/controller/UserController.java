6770bd0eb189873f672e41b7c4a0507c5e0e8e58
jeongjuhwang <hjj950303@naver.com>
2023-03-22 17:32:32 +0900

    remove: unused import

--- backend/src/main/java/com/temp/hwilyric/user/controller/UserController.java
+++ backend/src/main/java/com/temp/hwilyric/user/controller/UserController.java
@@ -22,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
