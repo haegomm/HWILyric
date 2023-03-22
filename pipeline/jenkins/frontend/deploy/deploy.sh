eefd87bb8384801dfd06f54549f3390d51f7ea8d
손민혁 <sonmh79@naver.com>
2023-03-23 00:03:02 +0900

    fix: modify path in deploy script

--- pipeline/jenkins/frontend/deploy/deploy.sh
+++ pipeline/jenkins/frontend/deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "***DEPLOYING DOCKER CONTAINERS***"
 echo "*********************************"
 
 # docker container 실행
-cd /var/jenkins_home/workspace/frontend-pipeline/pipeline/frontend
+cd /var/jenkins_home/workspace/frontend-pipeline/pipeline/jenkins/frontend
 docker compose up -d
 
 # dangling images 삭제
