5b131da394dc59762faabfb6a7ca2861eccffc85
leegaeun <rkdms4054@gmail.com>
2023-03-24 13:40:39 +0900

    feat: add title logic

--- frontend/hwilyric/src/components/write/features/BlockList.tsx
+++ frontend/hwilyric/src/components/write/features/BlockList.tsx
@@ -1,6 +1,32 @@
+import { useRef } from "react"
+import { useRecoilState } from "recoil"
+import { titleState,  } from "../../../atoms/BlockAtoms"
+
 function BlockList() {
+    const titleInput = useRef<HTMLInputElement>(null)
+    const [ title, setWTitle] = useRecoilState(titleState)
+
+    const handleChangeTitle = () => {
+        if (titleInput.current) {
+            setWTitle(titleInput.current.value)
+            console.log("제목이 뭐야?", title )
+        }
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <input
+                ref={titleInput}
+                value={title}
+                placeholder={"무제"}
+                onChange={handleChangeTitle}
+            />
+            <button onClick={handleSubmit}>저장</button>
+        </div>
     )
 }
 
