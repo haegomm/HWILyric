20b02a8c473d4a3210b6e7d6689c91352fa2aee5
leegaeun <rkdms4054@gmail.com>
2023-03-24 13:39:28 +0900

    feat: add createblock logic

--- frontend/hwilyric/src/components/write/features/BlockCreate.tsx
+++ frontend/hwilyric/src/components/write/features/BlockCreate.tsx
@@ -1,6 +1,45 @@
+import { useState } from "react"
+import { useRecoilState } from "recoil"
+import { BlockData, blockListState, blockIdState } from "../../../atoms/BlockAtoms"
+import BlockList from "./BlockList"
+
 function BlockCreate() {
+    const [ blockList, setBlockList ] = useRecoilState<BlockData[]>(blockListState)
+    const [ blockId, setBlockId ] = useRecoilState<number>(blockIdState)
+
+    const [ blockType, setBlockType ] = useState<string>()
+
+    const selectBlockType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setBlockType(event.target.value)
+    }
+
+    const onCreate = (): void => {
+        const newBlock: BlockData = {
+            index: blockId,
+            type: blockType,
+            lyrics: []
+        }
+        setBlockId(() => blockId + 1)
+        setBlockList(() => [newBlock, ...blockList])
+    }
+
     return (
-        <div></div>
+        <div>
+            <div className="BlockListBox">
+                { blockList ? (<BlockList />) : (<></>) }
+            </div>
+            <div className="addBlockBox">
+                <select
+                    onChange={selectBlockType}>
+                    <option value={"Verse 1"}>verse 1</option>
+                    <option value={"Verse 2"}>verse 2</option>
+                    <option value={"Bridge"}>Bridge</option>
+                    <option value={"Hook"}>Hook</option>
+                    <option value={"Etc"}>Etc</option>
+                </select>
+                <button onClick={onCreate}> + </button>
+            </div>
+        </div>
     )
 }
 
