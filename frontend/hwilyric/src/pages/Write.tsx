8a36273530e6dfd0cd5a3b960a04db607f5d4a9c
leegaeun <rkdms4054@gmail.com>
2023-03-25 20:51:25 +0900

    feat: add note components

--- frontend/hwilyric/src/pages/Write.tsx
+++ frontend/hwilyric/src/pages/Write.tsx
@@ -1,7 +1,14 @@
+import BlockCreate from "../components/write/features/BlockCreate"
+import BlockList from "../components/write/features/BlockList"
+import NoteTitle from "../components/write/features/NoteTitle"
+
 function Write() {
   return (
       <div>
         <h1>작사하기 페이지 입니다.</h1>
+        <NoteTitle />
+        <BlockList />
+        <BlockCreate />
       </div>
   )
 }
