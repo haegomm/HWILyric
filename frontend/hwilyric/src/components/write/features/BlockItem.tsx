baf86ea00387dbcbb073027fb9e298747f7e06a9
leegaeun <rkdms4054@gmail.com>
2023-03-27 01:35:46 +0900

    feat: add draggable feature and props type

--- frontend/hwilyric/src/components/write/features/BlockItem.tsx
+++ frontend/hwilyric/src/components/write/features/BlockItem.tsx
@@ -1,36 +1,46 @@
 import { useCallback, useRef, useState } from "react"
+import { Draggable } from "react-beautiful-dnd"
 import { useRecoilState } from "recoil"
 import { blockListState, BlockData } from "../../../atoms/BlockAtoms"
 
-function BlockItem({ id, type, lyrics }: BlockData) {
+interface BlockItemProps {
+    block: BlockData
+    index: number
+}
+
+function BlockItem({ block, index}: BlockItemProps) {
 
     const [blockList, setBlockList] = useRecoilState(blockListState)
-    const index = blockList.findIndex((block) => block.id === id)
 
     const [blockType, setBlockType] = useState<string>()
+    const [lyrics, setLyrics] = useState<string>("")
 
     const ref = useRef<HTMLTextAreaElement>(null)
 
-    const replaceIndex = (blockList: BlockData[], index: number, editedBlock: BlockData) => {
-        return [...blockList.slice(0, index), editedBlock, ...blockList.slice(index + 1)]
-    }
-
-    const selectBlockType = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const onChangeBlockType = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setBlockType(event.target.value)
     }
 
-    const onEditLyrics = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const editedBlock = {
-            id,
+    const onChangeLyrics = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setLyrics(event.target.value)
+        console.log(lyrics)
+    }
+
+    // 자동저장 or 저장 버튼 눌렸을 때
+    const onEditBlocks = () => {
+        let newLyrics: string = lyrics
+        newLyrics = newLyrics.replaceAll("<br>", "\r\n")
+
+        const newBlockList = blockList.map((it) => it.id === block.id ? {
+            ...it,
             type: blockType,
-            lytics: event.target.value,
-        }
-        const newList = replaceIndex(blockList, index, editedBlock)
-        setBlockList(newList)
+            lyrics: newLyrics
+        } : it)
+        setBlockList(newBlockList)
     }
 
     const onDeleteBlock = () => {
-        const newBlockList = blockList.filter((block) => block.id !== id)
+        const newBlockList = blockList.filter((item) => item.id !== block.id)
         setBlockList(newBlockList)
     }
 
@@ -43,25 +53,32 @@ function BlockItem({ id, type, lyrics }: BlockData) {
     }, [])
 
     return (
-        <div>
-            <select
-                defaultValue={type}
-                onChange={selectBlockType}>
-                <option value={"Verse 1"}>verse 1</option>
-                <option value={"Verse 2"}>verse 2</option>
-                <option value={"Bridge"}>Bridge</option>
-                <option value={"Hook"}>Hook</option>
-                <option value={"Etc"}>Etc</option>
-            </select>
-            <textarea
-                className="writeLyric"
-                value={lyrics}
-                ref={ref}
-                onChange={onEditLyrics}
-                onInput={handleResizeHeight}
-            />
-            <button onClick={onDeleteBlock}>-</button>
-        </div>
+        <Draggable draggableId={block.id.toString()} index={index}>
+            {(provided) => (
+                <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}>
+                    <select
+                        defaultValue={block.type}
+                        onChange={onChangeBlockType}>
+                        <option value={"Verse 1"}>verse 1</option>
+                        <option value={"Verse 2"}>verse 2</option>
+                        <option value={"Bridge"}>Bridge</option>
+                        <option value={"Hook"}>Hook</option>
+                        <option value={"Etc"}>Etc</option>
+                    </select>
+                    <textarea
+                        className="writeLyric"
+                        value={lyrics}
+                        ref={ref}
+                        onChange={onChangeLyrics}
+                        onInput={handleResizeHeight}
+                    />
+                    <button onClick={onDeleteBlock}>-</button>
+                </div>
+            )}
+        </Draggable>
     )
 }
 
