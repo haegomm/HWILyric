f5ad3484e2759c37b7fbbf392e37b593264e9be9
leegaeun <rkdms4054@gmail.com>
2023-03-29 15:05:16 +0900

    feat: Add blockid default value to current time

--- frontend/hwilyric/src/atoms/NoteAtoms.ts
+++ frontend/hwilyric/src/atoms/NoteAtoms.ts
@@ -1,22 +1,39 @@
 import { atom } from "recoil"
+import { recoilPersist } from 'recoil-persist';
 import { IBlockData } from "../types/noteType"
+
+const { persistAtom } = recoilPersist();
+
+function getCurrentDateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const id = `${year}${month}${day}${hour}${minute}`;
+    return Number(id);
+  }
 
 export const noteIdState = atom<string | null>({
     key: "noteIdState",
-    default: null
+    default: null,
+    effects_UNSTABLE: [persistAtom],
 })
 
 export const titleState = atom<string>({
     key: "titleState",
-    default: "무제"
+    default: "무제",
+    effects_UNSTABLE: [persistAtom],
 })
 
 export const blockIdState = atom<number>({
     key: 'blockIdState',
-    default: 0
+    default: getCurrentDateTime()
 })
 
 export const blockListState = atom<IBlockData[]>({
     key: 'blockListState',
-    default: []
+    default: [],
+    effects_UNSTABLE: [persistAtom],
 })
