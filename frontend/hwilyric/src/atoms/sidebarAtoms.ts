95bc78ac7f496f73235fc607f56b7246d96881cd
leegaeun <rkdms4054@gmail.com>
2023-03-29 15:06:52 +0900

    feat: memo persists on refresh

--- frontend/hwilyric/src/atoms/sidebarAtoms.ts
+++ frontend/hwilyric/src/atoms/sidebarAtoms.ts
@@ -1,6 +1,10 @@
 import { atom } from "recoil"
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const memoState = atom<string>({
     key: "memoState",
-    default: ""
+    default: "",
+    effects_UNSTABLE: [persistAtom],
 })
