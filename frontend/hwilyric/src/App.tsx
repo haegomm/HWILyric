b415ce4412e38f45bec9ba9639a65ebe5c8dd250
dokkyunYU <ydgar001@naver.com>
2023-03-29 21:12:53 +0900

    chore : rebase dev-front를 하기 위해 기존의 모든 변경 사항 커밋

--- frontend/hwilyric/src/App.tsx
+++ frontend/hwilyric/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from 'react-router';
-import { useRecoilValue } from "recoil"
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { useRecoilValue } from "recoil";
 
 import './App.css';
 import Navbar from './components/common/Navbar';
@@ -17,16 +17,19 @@ import HWILyric from './pages/Hwilyric';
 import { IsLoginAtom } from './atoms/userAtom';
 import userApi from './api/userApi';
 
-function App() {
-  const isLogin = useRecoilValue(IsLoginAtom)
+import "./App.css";
+import DataVisualize from "./pages/DataVisualize";
+import userAtom from "./atoms/userAtom";
 
-  
+function App() {
+  const isLogin = useRecoilValue(userAtom.IsLoginAtom);
+
   useEffect(() => {
     async function reissueToken() {
-      const accessToken = await userApi.reissueToken()
+      const accessToken = await userApi.reissueToken();
       if (accessToken !== null) {
-        console.log('토큰 재발급~~')
-        window.localStorage.setItem('accessToken', accessToken)
+        console.log("토큰 재발급~~");
+        window.localStorage.setItem("accessToken", accessToken);
       } else {
         console.log("토큰재발급 실패ㅜㅜ");
       }
@@ -39,22 +42,25 @@ function App() {
   }, []);
 
   return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Routes>
-            <Route path='*' element={<HWILyric />} />
-            <Route path='/zslkdrj' element={<Home />} />
-            <Route path='/write/sdalkfjadslkfj' element={<Write />} />
-            {/* <Route path='/datavisualize' element={<DataVisualize />} /> */}
-            <Route path='/login/dlkfjsaldkfj' element={<Login />} />
-            <Route path="oauth2/code/kakao" element={<LoginKakao />} />
-            <Route path='/signup/dkfjdlksj' element={<Signup />} />
-            <Route path='/mypage/dsajhfawjehdg' element={<Mypage />} />
-            <Route path='/profilemodification/sajhdgdjakhsd' element={<ProfileModification />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="*" element={<HWILyric />} />
+          <Route path="/zslkdrj" element={<Home />} />
+          <Route path="/write/sdalkfjadslkfj" element={<Write />} />
+          <Route path="/datavisualize" element={<DataVisualize />} />
+          <Route path="/login/dlkfjsaldkfj" element={<Login />} />
+          <Route path="oauth2/code/kakao" element={<LoginKakao />} />
+          <Route path="/signup/dkfjdlksj" element={<Signup />} />
+          <Route path="/mypage/dsajhfawjehdg" element={<Mypage />} />
+          <Route
+            path="/profilemodification/sajhdgdjakhsd"
+            element={<ProfileModification />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const WeeklyReportDiv = styled.div`
+  width: 368px;
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const WeeklyReportTitle = styled.title`
+  display: block;
+  width: 312px;
+  height: 152px;
+  font-size: large;
+`;
+
+export const WeeklyReportSubtitle = styled.div`
+  display: block;
+  width: 256px;
+  height: 248px;
+`;
