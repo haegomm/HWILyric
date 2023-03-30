95c4c24bf301bd825d80e641857714d85a7a0788
leegaeun <rkdms4054@gmail.com>
2023-03-31 04:05:07 +0900

    feat: import title style

--- frontend/hwilyric/src/components/write/note/NoteTitle.tsx
+++ frontend/hwilyric/src/components/write/note/NoteTitle.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react"
 import { useRecoilState } from "recoil"
 import { titleState } from "../../../atoms/noteAtoms"
+import { TitleDivBox } from "../../../styles/writeNoteStyle"
 
 function NoteTitle() {
     const titleInput = useRef<HTMLInputElement>(null)
@@ -14,14 +15,14 @@ function NoteTitle() {
     }
 
     return (
-        <div>
+        <TitleDivBox>
           <input
             ref={titleInput}
             value={title}
             placeholder={"무제"}
             onChange={handleChangeTitle}
           />
-        </div>
+        </TitleDivBox>
     )
 }
 
