4c2696c91cfac14da66171429f5a5acf1bee6fa0
dokkyunYU <ydgar001@naver.com>
2023-03-30 14:11:42 +0900

    feat : 데이터 시각화 스타일

--- frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
+++ frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+// DataVisualizePage
+export const DataVisualizePage = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 1920px;
+  height: 800px;
+`;
+
+// WeeklyReport Component
 export const WeeklyReportDiv = styled.div`
   width: 368px;
   height: 800px;
@@ -20,4 +29,26 @@ export const WeeklyReportSubtitle = styled.div`
   display: block;
   width: 256px;
   height: 248px;
+`;
+
+// Annual Report Component
+
+export const AnnualReportDiv = styled.div`
+  width: 420px;
+  height: 800px;
+`;
+
+export const AnnualGenreDiv = styled.div`
+  width: 100px;
+  height: 100px;
+`;
+
+export const AnnualGenreChartDiv = styled.div`
+  width: 100px;
+  height: 100px;
+`;
+
+export const AnnualKeywordDiv = styled.div`
+  width: 300px;
+  height: 300px;
 `;
