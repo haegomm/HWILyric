efe90e742dd90b64f47d58668603b687c0712b30
leegaeun <rkdms4054@gmail.com>
2023-03-30 16:52:49 +0900

    feat: add theme button

--- frontend/hwilyric/src/pages/Home.tsx
+++ frontend/hwilyric/src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import HomeContent from '../components/home/HomeContent';
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
-function Home() {
+function Home({ isDarkMode, toggleDarkMode }: any) {
     return (
         <div>
+        <button
+            type="button"
+            onClick={toggleDarkMode}
+        >
+            {isDarkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
+        </button>
         <h1>HWILyric</h1>
             <HomeContent />
         </div>        
