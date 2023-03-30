68c416beaab8c7a1731e59fb8cfd4a4340dfb674
leegaeun <rkdms4054@gmail.com>
2023-03-31 04:01:37 +0900

    feat: add darkmode button

--- frontend/hwilyric/src/components/common/Navbar.tsx
+++ frontend/hwilyric/src/components/common/Navbar.tsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom"
 import { useRecoilValue } from "recoil"
+import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
-import { IsLoginAtom, userNicknameAtom }from "../../atoms/userAtom"
 
-function Navbar() {
+import { IsLoginAtom, userNicknameAtom } from "../../atoms/userAtom"
+import { ToggleBoxWrapper, ToggleBox, ToggleBoxLabel } from '../../styles/toggleButton'
+
+function Navbar({toggleDarkMode}: any) {
   const isLogin = useRecoilValue(IsLoginAtom)
   const nickname = useRecoilValue(userNicknameAtom)
   return (
@@ -21,6 +24,12 @@ function Navbar() {
           <NavLink to="/login">로그인</NavLink>
         )}
       </div>
+      <BsFillSunFill />
+      <ToggleBoxWrapper>
+          <ToggleBox id="checkbox" type="checkbox" onClick={toggleDarkMode} />
+          <ToggleBoxLabel htmlFor="checkbox" />
+      </ToggleBoxWrapper>
+      <BsFillMoonFill />
     </nav>
   )
 }
