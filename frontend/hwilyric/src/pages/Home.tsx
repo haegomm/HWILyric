ca22017eff0309a206f37baa9cab0b5a53c92ce3
leegaeun <rkdms4054@gmail.com>
2023-03-31 04:06:05 +0900

    refactor: delete darkmode button

--- frontend/hwilyric/src/pages/Home.tsx
+++ frontend/hwilyric/src/pages/Home.tsx
@@ -1,15 +1,8 @@
 import HomeContent from '../components/home/HomeContent';
-import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs';
 
-function Home({ isDarkMode, toggleDarkMode }: any) {
+function Home() {
     return (
         <div>
-        <button
-            type="button"
-            onClick={toggleDarkMode}
-        >
-            {isDarkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
-        </button>
         <h1>HWILyric</h1>
             <HomeContent />
         </div>        
