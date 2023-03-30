32f455d9cd07e0419bf2d629dbfee5f38ab17a76
dokkyunYU <ydgar001@naver.com>
2023-03-30 14:28:37 +0900

    feat  : 시각화 스타일

--- frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
+++ frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
@@ -1,31 +1,18 @@
 import styled from "styled-components";
 
-<<<<<<< HEAD
-// DataVisualizePage
-export const DataVisualizePage = styled.div`
-  display: flex;
-  justify-content: space-around;
-  width: 1920px;
-  height: 800px;
-`;
+export const DataVisualizePage = styled.div``;
 
-// WeeklyReport Component
-=======
->>>>>>> 720f08a9b76022b8b6a90d1a292619d2e2a5452c
 export const WeeklyReportDiv = styled.div`
+  display: block;
   width: 368px;
   height: 800px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
 `;
 
 export const WeeklyReportTitle = styled.title`
   display: block;
+  margin-left: 112px;
   width: 312px;
   height: 152px;
-  font-size: large;
 `;
 
 export const WeeklyReportSubtitle = styled.div`
@@ -33,28 +20,5 @@ export const WeeklyReportSubtitle = styled.div`
   width: 256px;
   height: 248px;
 `;
-<<<<<<< HEAD
 
-// Annual Report Component
-
-export const AnnualReportDiv = styled.div`
-  width: 420px;
-  height: 800px;
-`;
-
-export const AnnualGenreDiv = styled.div`
-  width: 100px;
-  height: 100px;
-`;
-
-export const AnnualGenreChartDiv = styled.div`
-  width: 100px;
-  height: 100px;
-`;
-
-export const AnnualKeywordDiv = styled.div`
-  width: 300px;
-  height: 300px;
-`;
-=======
->>>>>>> 720f08a9b76022b8b6a90d1a292619d2e2a5452c
+export const AnnualGenreDiv = styled.div``;
