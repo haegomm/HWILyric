fdc8f87900b14d8605c80ae358f92707b7a4237f
leegaeun <rkdms4054@gmail.com>
2023-04-01 17:20:07 +0900

    feat: Add navbar div style

---
+++ frontend/hwilyric/src/styles/common/NavbarStyle.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const NavBox = styled.div`
+    display: flex;
+    backgound-color: none;
+    height: 12vh;
+    padding: 0;
+`
+
+export const NavMenu = styled.div`
+    display: flex;
+    justify-content : center;
+    align-items : center;
+    whidth: 36vw;
+`
