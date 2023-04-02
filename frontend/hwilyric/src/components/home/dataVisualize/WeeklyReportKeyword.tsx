44b7c4775e053978f6aa7470e4cf9053d39f9311
dokkyunYU <ydgar001@naver.com>
2023-04-03 01:13:02 +0900

    feat: 키워드 반복시 색깔 자동 변경

--- frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportKeyword.tsx
+++ frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportKeyword.tsx
@@ -1,38 +1,45 @@
 import React from "react";
 import {
-  WeeklyGenreBlockP,
-  WeeklyGenreLineDiv,
+  WeeklyKeywordBlockP,
+  WeeklyKeywordLineDiv,
   WeeklyReportGenreDiv,
 } from "../../../styles/DataVisaulizeStyle";
-import { IWeeklyReportGenre } from "../../../types/visualizingType";
+import { IWeeklyReportKeyword } from "../../../types/visualizingType";
 
 function WeeklyReportKeyword(props: any) {
   // const data = props.data
-  const data: IWeeklyReportGenre[] = [
-    { name: "장르1", count: 1 },
-    { name: "장르2", count: 2 },
-    { name: "장르3", count: 3 },
-    { name: "장르4", count: 4 },
-    { name: "장르5", count: 5 },
-    { name: "장르6", count: 6 },
+  const data: IWeeklyReportKeyword[] = [
+    { name: "키워드1", count: 1 },
+    { name: "키워드2", count: 2 },
+    { name: "키워드3", count: 3 },
+    { name: "키워드4", count: 4 },
+    { name: "키워드5", count: 5 },
+    { name: "키워드6", count: 6 },
   ];
 
-  // 장르 데이터를 그룹화하여 3개씩 나누기
+  const keywordBlockColors = ["#B0E3F9", "#DEB3FB", "#FEC3B5", "#FBD5E0"];
+
+  // 키워드 데이터를 그룹화하여 3개씩 나누기
   const groupedData = [];
   for (let i = 0; i < 6; i += 3) {
-    const mydata = props.data ? props.data : data;
+    const mydata = props.data.length !== 0 ? props.data : data;
     groupedData.push(mydata.slice(i, i + 3));
   }
-
+  let colorNum = -1;
   return (
     <WeeklyReportGenreDiv>
       {groupedData.map((group) => {
         return (
-          <WeeklyGenreLineDiv>
+          <WeeklyKeywordLineDiv>
             {group.map((genreData: any) => {
-              return <WeeklyGenreBlockP>{genreData.name}</WeeklyGenreBlockP>;
+              colorNum++;
+              return (
+                <WeeklyKeywordBlockP color={keywordBlockColors[colorNum % 4]}>
+                  {genreData.name}
+                </WeeklyKeywordBlockP>
+              );
             })}
-          </WeeklyGenreLineDiv>
+          </WeeklyKeywordLineDiv>
         );
       })}
     </WeeklyReportGenreDiv>
--- frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
+++ frontend/hwilyric/src/styles/DataVisaulizeStyle.ts
@@ -12,6 +12,16 @@ export const WeeklyReportDiv = styled.div`
   align-items: center;
   width: 368px;
   height: 800px;
+  border-style: solid;
+  border-width: 1px 1px 1px 1px;
+  border-image: linear-gradient(
+      to right bottom,
+      #ace5f8,
+      #e6b2fd,
+      #fdbfb4,
+      #fbd1dd
+    )
+    1;
 `;
 
 export const WeeklyReportTitle = styled.title`
@@ -49,21 +59,48 @@ export const AnnualReportDiv = styled.div`
   height: 800px;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  justify-content: space-between;
+`;
+
+export const AnnualReportTitle = styled.title`
+  font-size: 25px;
+  display: flex;
+  justify-content: center;
+  align-item: center;
 `;
 
 export const AnnualGenreDiv = styled.div`
   width: 420px;
-  height: 320px;
+  height: 380px;
   display: flex;
   justify-content: space-around;
   flex-direction: column;
   align-items: center;
+  border-style: solid;
+  border-width: 2px 2px 2px 2px;
+  border-image: linear-gradient(
+      to right top,
+      #b0e3f9,
+      #deb3fb,
+      #fec3b5,
+      #fbd5e0
+    )
+    2;
 `;
 
 export const TotalTrendDiv = styled.div`
   width: 1352px;
   height: 800px;
+  border-style: solid;
+  border-width: 2px 2px 2px 2px;
+  border-image: linear-gradient(
+      to right top,
+      #b0e3f9,
+      #deb3fb,
+      #fec3b5,
+      #fbd5e0
+    )
+    2;
 `;
 
 export const AnnualKeywordDiv = styled.div`
@@ -72,15 +109,25 @@ export const AnnualKeywordDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  border-style: solid;
+  border-width: 2px 2px 2px 2px;
+  border-image: linear-gradient(
+      to right top,
+      #b0e3f9,
+      #deb3fb,
+      #fec3b5,
+      #fbd5e0
+    )
+    2;
 `;
 
-export const WeeklyGenreLineDiv = styled.div`
+export const WeeklyKeywordLineDiv = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
 `;
 
-export const WeeklyGenreBlockP = styled.p`
+export const WeeklyKeywordBlockP = styled.p`
   width: 88px;
   height: 32px;
   display: flex;
@@ -88,6 +135,7 @@ export const WeeklyGenreBlockP = styled.p`
   justify-content: center;
   align-items: center;
   font-size: 15px;
+  background-color: ${(props) => props.color};
 `;
 
 export const AnnualGenreSongDiv = styled.div`
@@ -97,5 +145,5 @@ export const AnnualGenreSongDiv = styled.div`
   align-items: center;
   font-size: 20px;
   width: 420px;
-  height: 70px;
+  height: 60px;
 `;
