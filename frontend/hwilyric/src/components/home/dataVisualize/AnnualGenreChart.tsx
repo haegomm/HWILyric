42194e34b4c7843bea9fd83ed11425cee9cf9b9c
dokkyunYU <ydgar001@naver.com>
2023-04-03 01:15:02 +0900

    feat : 연간 장르 css 및 제목 추가

--- frontend/hwilyric/src/components/home/dataVisualize/AnnualGenreChart.tsx
+++ frontend/hwilyric/src/components/home/dataVisualize/AnnualGenreChart.tsx
@@ -3,11 +3,16 @@ import { Pie } from "react-chartjs-2";
 import { useSpring, animated } from "react-spring";
 import { ResponsivePie } from "@nivo/pie";
 import { getAllJSDocTagsOfKind } from "typescript";
-import { useSetRecoilState } from "recoil";
-import { annualNowSongAtom } from "../../../atoms/visualizingAtoms";
+import { useSetRecoilState, useRecoilValue } from "recoil";
+import {
+  annualNowAtom,
+  annualNowSongAtom,
+} from "../../../atoms/visualizingAtoms";
 import { annualGenreRatioSongAtom } from "../../../atoms/visualizingGenreAtom";
+import { AnnualReportTitle } from "../../../styles/DataVisaulizeStyle";
 
 function AnnualGenreChart(props: any) {
+  const annualnow = useRecoilValue(annualNowAtom);
   const setAnnualnowSong = useSetRecoilState(annualNowSongAtom);
   function myFunc(node: any, event: Object) {
     if (node) {
@@ -15,7 +20,8 @@ function AnnualGenreChart(props: any) {
     }
   }
   return (
-    <div style={{ width: "400px", height: "300px" }}>
+    <div style={{ width: "232px", height: "240px" }}>
+      <AnnualReportTitle>{annualnow}년대 인기 장르</AnnualReportTitle>
       <ResponsivePie
         data={props.data}
         margin={{ top: 10 }}
@@ -29,7 +35,10 @@ function AnnualGenreChart(props: any) {
           from: "color",
           modifiers: [["darker", 0.2]],
         }}
-        arcLinkLabelsSkipAngle={10}
+        arcLabel="id"
+        enableArcLinkLabels={false}
+        arcLinkLabelsSkipAngle={20}
+        arcLabelsRadiusOffset={0.6}
         arcLinkLabelsTextColor="#333333"
         arcLinkLabelsThickness={2}
         arcLinkLabelsColor={{ from: "color" }}
@@ -38,26 +47,6 @@ function AnnualGenreChart(props: any) {
           from: "color",
           modifiers: [["darker", 2]],
         }}
-        // defs={[
-        //   {
-        //     id: "dots",
-        //     type: "patternDots",
-        //     background: "inherit",
-        //     color: "rgba(255, 255, 255, 0.3)",
-        //     size: 4,
-        //     padding: 1,
-        //     stagger: true,
-        //   },
-        //   {
-        //     id: "lines",
-        //     type: "patternLines",
-        //     background: "inherit",
-        //     color: "rgba(255, 255, 255, 0.3)",
-        //     rotation: -45,
-        //     lineWidth: 6,
-        //     spacing: 10,
-        //   },
-        // ]}
         fill={[
           {
             match: {
