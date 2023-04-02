535adb02561f6cf58f91847579034549820751a5
dokkyunYU <ydgar001@naver.com>
2023-04-03 01:13:23 +0900

    feat : 장르 차트에 그라데이션 추가

--- frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportGenre.tsx
+++ frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportGenre.tsx
@@ -44,12 +44,22 @@ const data = [
 function WeeklyReportGenre(props: any) {
   return (
     <WeeklyReportKeywordsDiv>
-      <BarChart width={252} height={240} data={props.data ? props.data : data}>
+      <BarChart
+        width={252}
+        height={240}
+        data={props.data.length !== 0 ? props.data : data}
+      >
+        <defs>
+          <linearGradient id="colorBar" x1="0" y1="1" x2="0" y2="0">
+            <stop offset="40%" stopColor="#88C4E9" stopOpacity={0.7} />
+            <stop offset="80%" stopColor="#D1C4F1" stopOpacity={0.7} />
+          </linearGradient>
+        </defs>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" hide={true} />
         <YAxis hide={true} />
         <Tooltip />
-        <Bar dataKey="count" fill="#8884d8" />
+        <Bar dataKey="count" fill="url(#colorBar)" />
       </BarChart>
     </WeeklyReportKeywordsDiv>
   );
