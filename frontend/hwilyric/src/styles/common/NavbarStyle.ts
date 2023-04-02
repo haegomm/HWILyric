c3ade56f9b8c6c45352ce1e971d914067581555b
손민혁 <sonmh79@naver.com>
2023-04-02 19:54:57 +0900

    feat: add css for navbar

--- frontend/hwilyric/src/styles/common/NavbarStyle.ts
+++ frontend/hwilyric/src/styles/common/NavbarStyle.ts
@@ -2,14 +2,29 @@ import styled from "styled-components";
 
 export const NavBox = styled.div`
     display: flex;
+    justify-content: space-between;
+    align-items : center;
     backgound-color: none;
     height: 12vh;
     padding: 0;
+    margin: 0 5vw;
 `
 
 export const NavMenu = styled.div`
     display: flex;
-    justify-content : center;
     align-items : center;
-    whidth: 36vw;
+    width: 250px;
+    justify-content: space-between;
+`
+
+export const DarkModeBox = styled.div `
+    display: flex;
+    align-items : center;
+`
+
+export const GradientText = styled.span`
+    background: linear-gradient(45deg, rgba(251, 252, 185, 0.745), rgba(255, 205, 243, 0.667), rgba(101, 211, 355, 0.667));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
 `
