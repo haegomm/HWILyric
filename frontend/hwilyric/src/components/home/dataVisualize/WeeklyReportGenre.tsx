a220d8cc513049b9d7bf7c1782376d46e15362cd
dokkyunYU <ydgar001@naver.com>
2023-04-03 01:52:15 +0900

    fix : 주간 장르 X축 전부 표시 + 기울임

--- frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportGenre.tsx
+++ frontend/hwilyric/src/components/home/dataVisualize/WeeklyReportGenre.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { WeeklyReportKeywordsDiv } from "../../../styles/DataVisaulizeStyle";
+import { WeeklyReportGenreDiv } from "../../../styles/DataVisaulizeStyle";
 import {
   BarChart,
   CartesianGrid,
@@ -41,13 +41,53 @@ const data = [
   },
 ];
 
+// const CustomXAxisTick = (props: any) => {
+//   const { x, y, payload } = props;
+//   return (
+//     <g transform={`translate(${x},${y})`}>
+//       <text
+//         x={0}
+//         y={0}
+//         dy={16}
+//         textAnchor="end"
+//         fontSize={8} // 글자 크기 조절
+//         fill="#666" // 글자 색상
+//         transform="rotate(-35)"
+//       >
+//         {payload.value}
+//       </text>
+//     </g>
+//   );
+// };
+
+const CustomXAxisTick = (props: any) => {
+  const { x, y, payload } = props;
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor="end"
+        fontSize={10} // 글자 크기 조절
+        fill="#666" // 글자 색상
+        transform="rotate(-35)"
+      >
+        {payload.value}
+      </text>
+    </g>
+  );
+};
+
 function WeeklyReportGenre(props: any) {
   return (
-    <WeeklyReportKeywordsDiv>
+    <WeeklyReportGenreDiv>
       <BarChart
-        width={252}
+        width={304}
         height={240}
         data={props.data.length !== 0 ? props.data : data}
+        margin={{ top: 10, bottom: 10, left: 20, right: 20 }}
+        barCategoryGap={20}
       >
         <defs>
           <linearGradient id="colorBar" x1="0" y1="1" x2="0" y2="0">
@@ -56,12 +96,12 @@ function WeeklyReportGenre(props: any) {
           </linearGradient>
         </defs>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" hide={true} />
+        <XAxis dataKey="name" tick={<CustomXAxisTick />} interval={0} />
         <YAxis hide={true} />
         <Tooltip />
-        <Bar dataKey="count" fill="url(#colorBar)" />
+        <Bar dataKey="count" fill="url(#colorBar)" barSize={50} />
       </BarChart>
-    </WeeklyReportKeywordsDiv>
+    </WeeklyReportGenreDiv>
   );
 }
 
