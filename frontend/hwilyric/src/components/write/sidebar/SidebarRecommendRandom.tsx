2630fadc51156100e17860bb3504f363c9c162db
KooooY <8nya.nyanya8@gmail.com>
2023-04-03 09:25:45 +0900

    feat: add recommend tap

---
+++ frontend/hwilyric/src/components/write/sidebar/SidebarRecommendRandom.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidebarRecommendRandom() {
+  return (
+    <div>SidebarRecommendRandom</div>
+  )
+}
+
+export default SidebarRecommendRandom
---
+++ frontend/hwilyric/src/components/write/sidebar/SidebarRecommendRhyme.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SidebarRecommendRhyme() {
+  return (
+    <div>SidebarRecommendRhyme</div>
+  )
+}
+
+export default SidebarRecommendRhyme
