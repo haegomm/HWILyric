2c2cd4447ea30c4cdad8c9c15b64e360ccf2cb49
sonmh79 <sonmh79@naver.com>
2023-04-04 02:09:31 +0900

    feat: add music bar component

---
+++ frontend/hwilyric/src/components/write/MusicBar.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import "../../styles/music-bar.css"
+
+function MusicBar() {
+    return (
+        <div className="now playing" id="music">
+            <span className="bar n1">A</span>
+            <span className="bar n2">B</span>
+            <span className="bar n5">c</span>
+            {/* <span className="bar n4">D</span>
+            <span className="bar n5">E</span>
+            <span className="bar n6">F</span>
+            <span className="bar n7">G</span>
+            <span className="bar n8">H</span> */}
+      </div>
+    )
+}
+
+export default MusicBar
