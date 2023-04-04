897eba5c0f969a53ce1775108c32c1efed7eb258
손민혁 <sonmh79@naver.com>
2023-04-04 13:39:46 +0900

    fix: modify backend docker compose path in build file

--- pipeline/jenkins/backend/build/build.sh
+++ pipeline/jenkins/backend/build/build.sh
@@ -11,5 +11,5 @@ echo "***Building DOCKER CONTAINERS***"
 echo "********************************"
 
 # docker container 빌드
-cd /var/jenkins_home/workspace/pipeline-be/pipeline
+cd /var/jenkins_home/workspace/pipeline-be/pipeline/jenkins/backend
 docker compose build --no-cache
