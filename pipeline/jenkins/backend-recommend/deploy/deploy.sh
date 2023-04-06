6baf99e2eb1c7bec45c29c0421be425a3672a044
손민혁 <sonmh79@naver.com>
2023-04-07 01:52:14 +0900

    fix: add deploy script to remove exist container

--- pipeline/jenkins/backend-recommend/deploy/deploy.sh
+++ pipeline/jenkins/backend-recommend/deploy/deploy.sh
@@ -6,6 +6,8 @@ echo "*********************************"
 
 # docker container 실행
 cd /var/jenkins_home/workspace/pipeline-dj/pipeline/jenkins/backend-recommend
+docker stop hl-django
+docker rm hl-django
 docker compose up -d
 
 # dangling images 삭제
