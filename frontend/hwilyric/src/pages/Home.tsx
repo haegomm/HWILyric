e8ba62e7b2cb06e6111976c09165e59f54dba63e
dokkyunYU <ydgar001@naver.com>
2023-04-06 16:40:59 +0900

    feat : 데이터 시각화 뒷배경

--- frontend/hwilyric/src/pages/Home.tsx
+++ frontend/hwilyric/src/pages/Home.tsx
@@ -1,39 +1,42 @@
 import { useRecoilValue } from "recoil";
 
-import { useHorizontalScroll } from '../features/useHorizontalScroll';
-import { HomeContainer, HomeDiv, HomeContent, HomeTrapezoid } from '../styles/homeStyle';
-import HomeQuickview from '../components/home/HomeQuickview';
-import { IsLoginAtom } from '../atoms/userAtom';
-import HomeNotLoggedIn from '../components/home/HomeNotLoggedIn';
-import HomeCenter from '../components/home/HomeCenter';
-import { lightTrape } from '../assets/icon/myButtons';
+import { useHorizontalScroll } from "../features/useHorizontalScroll";
+import {
+  HomeContainer,
+  HomeDiv,
+  HomeContent,
+  HomeTrapezoid,
+} from "../styles/homeStyle";
+import HomeQuickview from "../components/home/HomeQuickview";
+import { IsLoginAtom } from "../atoms/userAtom";
+import HomeNotLoggedIn from "../components/home/HomeNotLoggedIn";
+import HomeCenter from "../components/home/HomeCenter";
+import { lightTrape } from "../assets/icon/myButtons";
 import { DataVisualizePage } from "../styles/DataVisaulizeStyle";
 import WeeklyReport from "../components/home/dataVisualize/WeeklyReport";
 import TotalTrend from "../components/home/dataVisualize/TotalTrend";
 import AnnualReport from "../components/home/dataVisualize/AnnualReport";
 
+import { useTheme } from "styled-components";
+
 function Home() {
-    const scrollRef = useHorizontalScroll();
-    const isLogin = useRecoilValue(IsLoginAtom);
-    return (
-        <HomeContainer ref={scrollRef}>
-            <HomeTrapezoid src={lightTrape} />
-            <HomeContent>
-                <HomeDiv>
-                    {isLogin ? 
-                        <HomeQuickview />
-                        : <HomeNotLoggedIn />
-                    }
-                </HomeDiv>
-                <HomeCenter />
-                <DataVisualizePage>
-                  <WeeklyReport />
-                  <TotalTrend />
-                  <AnnualReport />
-                </DataVisualizePage>
-            </HomeContent>
-        </HomeContainer>        
-    )
+  const theme = useTheme();
+  const scrollRef = useHorizontalScroll();
+  const isLogin = useRecoilValue(IsLoginAtom);
+  return (
+    <HomeContainer ref={scrollRef}>
+      <HomeTrapezoid src={lightTrape} />
+      <HomeContent>
+        <HomeDiv>{isLogin ? <HomeQuickview /> : <HomeNotLoggedIn />}</HomeDiv>
+        <HomeCenter />
+        <DataVisualizePage theme={theme}>
+          <WeeklyReport />
+          <TotalTrend />
+          <AnnualReport />
+        </DataVisualizePage>
+      </HomeContent>
+    </HomeContainer>
+  );
 }
 
 export default Home;
